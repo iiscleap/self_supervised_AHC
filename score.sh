943732fc40b8d51b1141eed7b016e49e91879457
LEAP Lab <leap.iisc@gmail.com>
2020-08-06 12:22:13 +0530

    Update score.sh

--- score.sh
+++ score.sh
@@ -1,6 +1,8 @@
-path=$1
-find $path/final_rttms/*.rttm > $path/$3
+#Usage: This script generate filewise DER , ignoring overlaps and collar : 0.25s
+# bash score.sh <path where rttm is stored> <file containing path of ground truth filewise rttm> 
 
-python services/dscore-master/score.py --ignore_overlaps --collar 0.25 -R $2 -S $path/$3 > $path/$4
-grep OVERALL $path/$4 
+path=$1
+find $path/final_rttms/*.rttm > $path/sys.scp
+python services/dscore-master/score.py --ignore_overlaps --collar 0.25 -R $2 -S $path/sys.scp > $path/der.scp 2> metrics_dev.stderr
+grep OVERALL $path/der.scp
 
