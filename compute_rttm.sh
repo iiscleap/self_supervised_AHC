7ff4f00fa642eedb066fba1d1569b2461d9f5c10
LEAP Lab <leap.iisc@gmail.com>
2020-08-06 12:10:29 +0530

    Update compute_rttm.sh

--- compute_rttm.sh
+++ compute_rttm.sh
@@ -1,3 +1,5 @@
+#Usage : this script generates filewise rttm 
+#bash compute_rttm.sh <dataset name> <path where rttm is generated> <rttm name>
 mkdir -p $2/final_rttms
 cat ../lists/$1/${1}.list | while read i; do
 grep $i $2/$3 > $2/final_rttms/${i}.rttm
